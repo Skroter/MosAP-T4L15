b1de6e1d10599a38f6c7f07adc2ab3ecbe60b61c
Skroter <digro@yandex.ru>
2024-07-01 15:40:48 +0500

    Добавьте файлы проекта.

---
+++ Topic4Lesson15Task1/Topic4Lesson15Task1.py
@@ -0,0 +1,19 @@
+﻿class Transport:
+    
+    def __init__(self, name, max_speed, mileage):
+        self.name = name
+        self.max_speed = max_speed
+        self.mileage = mileage
+
+class Bus(Transport):
+    def __init__(self, name, max_speed, mileage):
+        self.name = name
+        self.max_speed = max_speed
+        self.mileage = mileage
+        
+    def __str__(self):
+        return "Название автомобиля: "+self.name+" Скорость: "+self.max_speed+" Пробег: "+self.mileage
+    
+busVariable = Bus("Renaul Logan", "180", "12")
+
+print(busVariable)
